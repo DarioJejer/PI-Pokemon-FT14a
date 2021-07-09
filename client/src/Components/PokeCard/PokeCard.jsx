55d837b70c26383a2f4d0d27c4f8d3062bbf841d
Dario Jejer <dariojejer@gmail.com>
2021-07-09 02:51:24 -0300

    CreatePokeCardComponent

---
+++ client/src/Components/PokeCard/PokeCard.jsx
@@ -0,0 +1,26 @@
+import './PokeCard.css';
+import { Link } from 'react-router-dom';
+
+const pokemon = {
+    name: "bulbasaur",
+    img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    type: [
+      "grass",
+      "poison"
+    ]};
+
+function PokeCard() {
+    return (
+      <>
+        <div className="box">
+            <h3>Name: {pokemon.name}</h3>
+            <img src={pokemon.img} className="pokeImg"/>
+            {pokemon.type.map(type => 
+                <h3>Type: {type}</h3>
+            )}
+        </div>
+      </>
+    );
+  }
+  
+export default PokeCard;
