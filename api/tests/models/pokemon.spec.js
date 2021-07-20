c42b9dca90948363f073eaf72b8c4d900ec52f8c
Dario Jejer <dariojejer@gmail.com>
2021-07-20 06:37:41 -0300

    ImplementBackEndTests

--- api/tests/models/pokemon.spec.js
+++ api/tests/models/pokemon.spec.js
@@ -18,5 +18,24 @@ describe('Pokemon model', () => {
         Pokemon.create({ name: 'Pikachu' });
       });
     });
+    describe("Correct creation", function () {
+      it("should return the given pokemon on create method", function () {
+        return Pokemon.create({
+          name: "picachu",
+          url: "http://assets.stickpng.com/thumbs/580b57fcd9996e24bc43c325.png",
+          hp: 50,
+          attack: 10,
+          defense: 10,
+          speed: 5,
+          height: 15,
+          weight: 100,
+        }).then((pokemon) => {
+          expect(pokemon.name).to.equal("picachu");
+          expect(pokemon.url).to.equal("http://assets.stickpng.com/thumbs/580b57fcd9996e24bc43c325.png");
+          expect(pokemon.hp).to.equal(50);
+        });
+      });
+    });
   });
+  
 });
--- api/tests/routes/pokemon.spec.js
+++ api/tests/routes/pokemon.spec.js
@@ -2,7 +2,7 @@
 const { expect } = require('chai');
 const session = require('supertest-session');
 const app = require('../../src/app.js');
-const { Pokemon, conn } = require('../../src/db.js');
+const { Pokemon, Type, conn } = require('../../src/db.js');
 
 const agent = session(app);
 const pokemon = {
@@ -17,8 +17,58 @@ describe('Pokemon routes', () => {
   beforeEach(() => Pokemon.sync({ force: true })
     .then(() => Pokemon.create(pokemon)));
   describe('GET /pokemons', () => {
-    it('should get 200', () =>
-      agent.get('/pokemons').expect(200)
-    );
+    it('should respond with a status 200', (done) => {
+      return agent.get('/pokemons').expect(200).end(done());
+    });
+    it("should return a list of 40 pokemon", (done) => {
+      return agent.get("/pokemons")
+        .expect(200)
+        .then((res) => {
+          expect(res.body.length).to.equal(40);
+        })
+        .end(done());
+    });
+    it("responded object should have a property name", (done) => {
+      return agent.get("/pokemons")
+        .expect(200)
+        .then((res) => {
+          expect(res.body[0]).to.equal(40);
+        })
+        .end(done());
+    });
   });
+  describe("GET /pokemons?name=xxx", () => {
+    it("should respond with a status 400", (done) => {
+      return agent.get("/pokemons?name=xxx")
+        .expect(400)
+        .end(done())
+    });
+  });
+});
+describe('Pokemon routes', () => {
+  before(() => conn.authenticate()
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  }));
+  beforeEach(() => Type.sync({ force: true }));
+  describe("GET /types", () => {
+    it('responded objects should have correct values', function(done) {
+      return Type.create({
+        name: "fire",
+      })
+      .then(() => {
+        return Type.create({
+          name: "water",
+        })
+      })
+      .then(() => {
+        return agent.get('/types')
+      })
+      .then(types => {
+        expect(types.body[0].name).to.equal("fire");
+        expect(types.body[1].name).to.equal("water");
+      })
+      .end(done())
+    });
+});
 });
