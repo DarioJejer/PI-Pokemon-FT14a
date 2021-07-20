1e926dd2833f50af2a988973f8b9c2e4680a838d
Dario Jejer <dariojejer@gmail.com>
2021-07-20 06:54:20 -0300

    ImplementClickToSeeDetailsOnPokeCard

--- client/src/Components/PokeCard/PokeCard.jsx
+++ client/src/Components/PokeCard/PokeCard.jsx
@@ -1,9 +1,20 @@
+import { selectPokemonByName } from "../../Actions/mainAction";
 import "./PokeCard.css";
-// import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+import { connect } from "react-redux";
 
-function PokeCard({ pokemon }) { 
+function PokeCard(props) { 
+
+  const {pokemon} = props;
+  const {push} = useHistory();
+
+  const handlePokeCardClick = () => {
+    props.selectPokemonByName(pokemon.name);
+    push("pokemons/selectedPokemon");
+  }
+
   return (
-    <>
+    <button className="poke-card-button" onClick={handlePokeCardClick}>
       <div className="box">
         <h3>{CFL(pokemon.name)}</h3>
         <div className="image-box">
@@ -11,10 +22,18 @@ function PokeCard({ pokemon }) {
         </div>
         <h3> {pokemon.types.map(t => CFL(t)).join(` - `)} </h3>
       </div>
-    </>
+    </button>
   );
 }
 
 export const CFL = (name) => name[0].toUpperCase() + name.slice(1);
 
-export default PokeCard;
+
+const mapDispatchToProps = {
+  selectPokemonByName
+}
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(PokeCard);
