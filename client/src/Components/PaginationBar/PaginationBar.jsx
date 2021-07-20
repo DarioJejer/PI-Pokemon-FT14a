1c373307b9db1883d0c7f81d087a4419e2e800ed
Dario Jejer <dariojejer@gmail.com>
2021-07-20 02:10:03 -0300

    AddStylesForPaginationBar

--- client/src/Components/PaginationBar/PaginationBar.jsx
+++ client/src/Components/PaginationBar/PaginationBar.jsx
@@ -8,50 +8,53 @@ export const PokemonsPerPage = 12;
 function PaginationBar(props) { 
 
     useEffect(() => {
-      setPageNumber(1);
+      setSelectedPageNumber(1);
       props.selectPage(1);
     }, [props.filteredPokemons])
 
-    const [pageNumber, setPageNumber] = useState(1);
+    const [selectedPageNumber, setSelectedPageNumber] = useState(1);
     const numOfPagesOnBar = 3;
     let totalNumberOfPages = 0;
 
     const handleClick = (newPageNumber) => {
-        setPageNumber(newPageNumber);
+        setSelectedPageNumber(newPageNumber);
         props.selectPage(newPageNumber);
     }
 
     const renderPageNumeration = () => {
         let numeration = []
         props.filteredPokemons.map((_p,i)=> {
+            let pageNumber = (i/PokemonsPerPage)+1;
             if((i)%PokemonsPerPage===0)
-                numeration.push(<button key={i} onClick={()=>handleClick((i/PokemonsPerPage)+1)}>{(i/PokemonsPerPage)+1}</button>);
+                numeration.push(<button key={i} className={pageNumber===selectedPageNumber ? 'pagination-numbers active' : 'pagination-numbers' } onClick={()=>handleClick(pageNumber)}>{pageNumber}</button>);
         })
         totalNumberOfPages = numeration.length;
-        if(pageNumber > totalNumberOfPages - numOfPagesOnBar/2){
+        if(totalNumberOfPages === 1)
+          return [];
+        if(selectedPageNumber > totalNumberOfPages - numOfPagesOnBar/2){
             return numeration.splice(-numOfPagesOnBar);
         }
-        if(pageNumber > numOfPagesOnBar-1){
-            return numeration.slice(pageNumber-(numOfPagesOnBar-1), pageNumber+(numOfPagesOnBar-2));
+        if(selectedPageNumber > numOfPagesOnBar-1){
+            return numeration.slice(selectedPageNumber-(numOfPagesOnBar-1), selectedPageNumber+(numOfPagesOnBar-2));
         }        
         return numeration.slice(0,numOfPagesOnBar);
     }
 
     const renderPrevButton = () => {
         let result = [];
-        if(pageNumber > 1)
-         result.push(<button onClick={()=>handleClick(pageNumber-1)}>Prev</button>);       
-        if(pageNumber>(numOfPagesOnBar/2 + 1))
-            result.push(<span>...</span>);
+        if(selectedPageNumber > 1)
+         result.push(<button className="pagination-buttons" onClick={()=>handleClick(selectedPageNumber-1)}>Prev</button>);       
+        if(selectedPageNumber>(numOfPagesOnBar/2 + 1))
+            result.push(<span className="pagination-buttons">...</span>);
         return result;
     }
 
     const renderNextButton = () => {
         let result = [];
-        if(pageNumber<(totalNumberOfPages - numOfPagesOnBar/2))
-            result.push(<span>...</span>);
-        if(pageNumber < totalNumberOfPages)
-            result.push(<button onClick={()=>handleClick(pageNumber+1)}>Next</button>);       
+        if(selectedPageNumber<(totalNumberOfPages - numOfPagesOnBar/2))
+            result.push(<span className="pagination-buttons">...</span>);
+        if(selectedPageNumber < totalNumberOfPages)
+            result.push(<button className="pagination-buttons" onClick={()=>handleClick(selectedPageNumber+1)}>Next</button>);       
         return result;
     }
 
